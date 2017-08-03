da2dc050b712a56c9674294800ab30235296dd24
Dan Bikle <danbikle@outlook.com>
2017-08-03 16:25:58 -0700

    more

--- flaskr.py
+++ flaskr.py
@@ -6,7 +6,8 @@ export FLASK_DEBUG=1
 export PORT=5011
 python flaskr.py
 Other shell:
-curl localhost:5011/demo11
+curl localhost:5011/demo11.json
+curl localhost:5011/static/hello.json
 """
 
 import os
@@ -21,7 +22,7 @@ class Demo11(fr.Resource):
     my_k_s = 'hello'
     my_v_s = 'world'
     return {my_k_s: my_v_s}
-api.add_resource(Demo11, '/demo11')
+api.add_resource(Demo11, '/demo11.json')
 
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
