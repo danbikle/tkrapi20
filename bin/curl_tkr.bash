bb1fde6d45e4a0f76fc43efcf91af0beed6b12f2
Dan Bikle <danbikle@outlook.com>
2017-08-03 19:23:22 -0700

    more

--- bin/curl_tkr.bash
+++ bin/curl_tkr.bash
@@ -7,12 +7,25 @@
 # Note the user-agent arg.
 # I need to pass a string; variable with string fails
 
+TKR=IBM
 
+rm -f /tmp/curl_tkr.bash.cookiejar.txt
 
 /usr/bin/curl --verbose \
-	      --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36' \
-	      https://finance.yahoo.com/quote/IBM \
-	      > /tmp/tkr.html \
-	      2> /tmp/s.txt
+              --cookie-jar /tmp/curl_tkr.bash.cookiejar.txt \
+              --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36' \
+              https://finance.yahoo.com/quote/${TKR} \
+              > /tmp/tkr0.html \
+              2> /tmp/s0.txt
+
+sleep 1
+
+/usr/bin/curl --verbose \
+              --cookie     /tmp/curl_tkr.bash.cookiejar.txt \
+              --cookie-jar /tmp/curl_tkr.bash.cookiejar.txt \
+              --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36' \
+              https://finance.yahoo.com/quote/${TKR}/history?p=${TKR} \
+              > /tmp/tkr1.html \
+              2> /tmp/s1.txt
 
 exit
