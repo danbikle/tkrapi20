5b0cda8515a55c43a9563656c5af2d41231c9f96
Dan Bikle <danbikle@outlook.com>
2017-08-04 15:31:43 -0700

    more

--- py/request_tkr.py
+++ py/request_tkr.py
@@ -9,14 +9,15 @@ import pdb
 
 pdb.set_trace()
 
-tkr = '^GSPC'
+tkr          = '^GSPC'
 user_agent_s = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36'
-
-tkr_r = requests.get('https://www.python.org')
+url_s        = 'https://finance.yahoo.com/quote/'+tkr
+headers_d    = {'User-Agent': user_agent_s}
+tkr_r        = requests.get(url_s, headers=headers_d)
 print(tkr_r.status_code)
 
 html_s = tkr_r.content
-print(html_s[:55])
+print(html_s[:155])
 
 'bye'
 
