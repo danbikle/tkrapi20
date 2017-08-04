eb5cc49a2a596814f87e19fcefc004ac9738527e
Dan Bikle <danbikle@outlook.com>
2017-08-03 23:23:28 -0700

    more

--- bin/curl_tkr.bash
+++ bin/curl_tkr.bash
@@ -27,6 +27,16 @@ sleep 1
               https://finance.yahoo.com/quote/${TKR}/history?p=${TKR} \
               > /tmp/tkr1.html \
               2> /tmp/s1.txt
+
+# In /tmp/tkr1.html I should see text like this near CrumbStore
+# "CrumbStore":{"crumb":"z6M4ACgDGXK"}
+
+# sed -n means ignore lines which do not match
+# I should match text left of 'CrumbStore'
+# I should match text right of '"}'
+crum=`sed -n '/CrumbStore/s/^.*CrumbStore":{"crumb":"//p' /tmp/tkr1.html | sed -n '1s/"}.*//p'`
+#sed -n '/CrumbStore/s/^.*CrumbStore":{"crumb":"//p' /tmp/tkr1.html|sed -n '1s/"}.*//p' > /tmp/c.txt
+
 sleep 1
 
 # https://query1.finance.yahoo.com/v7/finance/download/IBM?period1=-252432000&period2=1501743600&interval=1d&events=history&crumb=9cxzOy3G0UF
@@ -35,8 +45,8 @@ sleep 1
               --cookie     /tmp/curl_tkr.bash.cookiejar.txt \
               --cookie-jar /tmp/curl_tkr.bash.cookiejar.txt \
               --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36' \
-              https://query1.finance.yahoo.com/v7/finance/download/${TKR}?period1=-252432000&period2=1501743600&interval=1d&events=history \
-              > /tmp/tkr2.html \
+              "https://query1.finance.yahoo.com/v7/finance/download/${TKR}?period1=-252432000&period2=1501743600&interval=1d&events=history&crumb=${crum}" \
+              > /tmp/${TKR}.csv \
               2> /tmp/s2.txt
 
 exit
