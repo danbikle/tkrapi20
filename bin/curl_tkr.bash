f72ad55c1f51d5232a7020f0d981abb3bd3fc3e4
Dan Bikle <danbikle@outlook.com>
2017-08-04 04:03:28 -0700

    more

--- bin/curl_tkr.bash
+++ bin/curl_tkr.bash
@@ -8,7 +8,9 @@
 # I need to pass a string; variable with string fails
 
 TKR=IBM
-TKR=FB
+TKR=$1
+CSVDIR=/tmp/curl_tkr
+mkdir -p $CSVDIR
 
 rm -f /tmp/curl_tkr.bash.cookiejar.txt
 
@@ -19,7 +21,7 @@ rm -f /tmp/curl_tkr.bash.cookiejar.txt
               > /tmp/tkr0.html \
               2> /tmp/s0.txt
 
-sleep 1
+sleep 2
 
 /usr/bin/curl --verbose \
               --cookie     /tmp/curl_tkr.bash.cookiejar.txt \
@@ -38,19 +40,22 @@ sleep 1
 crum=`sed -n '/CrumbStore/s/^.*CrumbStore":{"crumb":"//p' /tmp/tkr1.html | sed -n '1s/"}.*//p'`
 #sed -n '/CrumbStore/s/^.*CrumbStore":{"crumb":"//p' /tmp/tkr1.html|sed -n '1s/"}.*//p' > /tmp/c.txt
 
-sleep 1
+sleep 2
 
 # https://query1.finance.yahoo.com/v7/finance/download/IBM?period1=-252432000&period2=1501743600&interval=1d&events=history&crumb=9cxzOy3G0UF
 
 # https://query1.finance.yahoo.com/v7/finance/download/%5EGSPC?period1=-631123200&period2=1501743600&interval=1d&events=history&crumb=9cxzOy3G0UF
 
+# I should get the current time as a unix time string:
 nowutime=`date +%s`
+
+# I should get the csv file now:
 /usr/bin/curl --verbose \
               --cookie     /tmp/curl_tkr.bash.cookiejar.txt \
               --cookie-jar /tmp/curl_tkr.bash.cookiejar.txt \
               --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36' \
               "https://query1.finance.yahoo.com/v7/finance/download/${TKR}?period1=-631123200&period2=${nowutime}&interval=1d&events=history&crumb=${crum}" \
-              > /tmp/${TKR}.csv \
+              > ${CSVDIR}/${TKR}.csv \
               2> /tmp/s2.txt
 
 exit
---
+++ bin/curl_tkrs.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# curl_tkrs.bash
+
+# This script should loop through a text file full of tkrs and feed each to curl_tkr.bash
+
+#cat ../tkrs.txt | while read TKR
+cat ../tkrlist.txt | while read TKR
+do
+    ./curl_tkr.bash $TKR
+    ls -la /tmp/curl_tkr/${TKR}.csv
+done
+
+exit
+
+./curl_tkr.bash AAPL
+./curl_tkr.bash IBM
+./curl_tkr.bash ^GSPC
+./curl_tkr.bash ^RUT
+
+exit
