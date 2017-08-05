4fdec4e5da27b568bb515634fc57c4ac25043c21
Dan Bikle <danbikle@outlook.com>
2017-08-04 19:39:11 -0700

    more

--- bin/request_tkr.bash
+++ bin/request_tkr.bash
@@ -3,8 +3,16 @@
 # request_tkr.bash
 
 # This script should call request_tkr.py
+# which should help me collect prices into CSV files
 
-cd ../py/
-python request_tkr.py AAPL
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+PARPATH=${SCRIPTPATH}/..
+PYPATH=${PARPATH}/py
+
+cat ${PARPATH}/tkrs.txt|while read TKR
+do
+  echo  python ${PYPATH}/request_tkr.py $TKR
+done
 
 exit
