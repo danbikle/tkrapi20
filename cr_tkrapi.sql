778803badb56509ab98a27543b661cc67f30026b
Dan Bikle <danbikle@outlook.com>
2017-08-12 14:36:15 -0700

    more

---
+++ cr_tkrapi.sql
@@ -0,0 +1,15 @@
+--
+-- cr_tkrapi.sql
+--
+
+-- This script should create both a role named tkrapi and db named tkrapi.
+-- Then I should be able to connect via python-sqlalchemy using this string:
+-- db_s = 'postgres://tkrapi:tkrapi@127.0.0.1/tkrapi'
+
+-- Demo:
+-- sudo su - postgres
+-- psql -f cr_tkrapi.sql
+
+create role tkrapi with login superuser password 'tkrapi';
+create database ttkrapi;
+
