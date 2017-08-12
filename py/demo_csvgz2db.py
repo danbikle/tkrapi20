ba3c830087a38b2cfea5d24c2ff7f514b88a0320
Dan Bikle <danbikle@outlook.com>
2017-08-11 22:55:37 -0700

    more

--- py/demo_csvgz2db.py
+++ py/demo_csvgz2db.py
@@ -6,9 +6,11 @@ This script should demo how to insert a csv.gz file into a table.
 Demo:
 ~/anaconda3/bin/python demo_csvgz2db.py
 """
+
 import pandas as pd
 import pdb
 from sqlalchemy import create_engine
+import glob
 
 # I should connect to the DB
 db_s = 'postgres://tkrapi:tkrapi@127.0.0.1/tkrapi'
@@ -17,27 +19,31 @@ conn = create_engine(db_s).connect()
 sql_s = "drop table if exists tkrprices"
 conn.execute(sql_s)
 
-sql_s = "create table tkrprices(tkr varchar,csv text)"
+sql_s = "create table tkrprices(tkr varchar, csv text)"
 conn.execute(sql_s)
 
-# I should read a small csv.gz file:
+# I should read csv.gz files:
+for csvf_s in glob.glob('/tmp/request_tkr/csv/*.csv.gz'):
+  tkr0_s = csvf_s.split('/')[-1].split('.')[0] # should be something like 'IBM'
+  csv_df = pd.read_csv(csvf_s)
+  # I should convert to String and pick only two columns:
+  csv0_s = csv_df.to_csv(index=False,header=False,columns=('Date','Close'),float_format='%.3f')
+  csv_s  = "'"+csv0_s+"'"
+  tkr_s  = "'"+tkr0_s+"'"
+  sql_s  = "insert into tkrprices(tkr,csv)values("+tkr_s+","+csv_s+")"
+  conn.execute(sql_s)
 
-csv_df = pd.read_csv('/tmp/request_tkr/csv/FB.csv.gz')
-tkrprices_df         = csv_df[['Date','Close']].sort_values(['Date'])
-tkrprices_df.columns = ['cdate','cp']
+# I should check:
+# sql_s = "select tkr, csv from tkrprices" # limit 1"
+# result = conn.execute(sql_s)
+# 
+# for row in result:
+#     print(row['tkr'],row['csv'].split(',')[-2:])
 
-# I should convert tkrprices_df into a CSV string:
-csv0_s = tkrprices_df.to_csv(index=False, float_format='%.3f')
-csv_s  = "'"+csv0_s+"'"
+# ../bin/psql.bash
+# select       tkr  from tkrprices;
+# select count(tkr) from tkrprices;
+# select csv from tkrprices where tkr='FB';
 
-sql_s = "insert into tkrprices(tkr,csv)values('FB',"+csv_s+")"
-conn.execute(sql_s)
+'bye'
 
-sql_s = "select csv from tkrprices limit 1"
-result = conn.execute(sql_s)
-# print(result)
-
-# <sqlalchemy.engine.result.ResultProxy object at 0x7fcbccdade80>
-
-for row in result:
-    print(row['csv'])
