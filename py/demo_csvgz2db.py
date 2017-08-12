e769c052ee09c690e5a748daf6cbe2711d22c14d
Dan Bikle <danbikle@outlook.com>
2017-08-11 19:14:14 -0700

    more

--- py/demo_csvgz2db.py
+++ py/demo_csvgz2db.py
@@ -17,25 +17,25 @@ conn = create_engine(db_s).connect()
 sql_s = "drop table if exists tkrprices"
 conn.execute(sql_s)
 
-sql_s = "create table tkrprices(csv text)"
+sql_s = "create table tkrprices(tkr varchar,csv text)"
 conn.execute(sql_s)
 
 # I should read a small csv.gz file:
-pdb.set_trace()
+
 csv_df = pd.read_csv('/tmp/request_tkr/csv/FB.csv.gz')
 tkrprices_df         = csv_df[['Date','Close']].sort_values(['Date'])
 tkrprices_df.columns = ['cdate','cp']
 
 # I should convert tkrprices_df into a CSV string:
-csv0_s = tkrprices_df.to_csv(index=False)
+csv0_s = tkrprices_df.to_csv(index=False, float_format='%.3f')
 csv_s  = "'"+csv0_s+"'"
 
-sql_s = "insert into tkrprices(csv)values("+csv_s+")"
+sql_s = "insert into tkrprices(tkr,csv)values('FB',"+csv_s+")"
 conn.execute(sql_s)
 
 sql_s = "select csv from tkrprices limit 1"
 result = conn.execute(sql_s)
-print(result)
+# print(result)
 
 # <sqlalchemy.engine.result.ResultProxy object at 0x7fcbccdade80>
 
