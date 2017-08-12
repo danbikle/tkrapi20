67ba55fb5eb159c154ef916dcf18849010cf920f
Dan Bikle <danbikle@outlook.com>
2017-08-11 18:25:06 -0700

    more

--- py/demo_csvgz2db.py
+++ py/demo_csvgz2db.py
@@ -6,7 +6,8 @@ This script should demo how to insert a csv.gz file into a table.
 Demo:
 ~/anaconda3/bin/python demo_csvgz2db.py
 """
-
+import pandas as pd
+import pdb
 from sqlalchemy import create_engine
 
 # I should connect to the DB
@@ -15,3 +16,28 @@ conn = create_engine(db_s).connect()
 
 sql_s = "drop table if exists tkrprices"
 conn.execute(sql_s)
+
+sql_s = "create table tkrprices(csv text)"
+conn.execute(sql_s)
+
+# I should read a small csv.gz file:
+pdb.set_trace()
+csv_df = pd.read_csv('/tmp/request_tkr/csv/FB.csv.gz')
+tkrprices_df         = csv_df[['Date','Close']].sort_values(['Date'])
+tkrprices_df.columns = ['cdate','cp']
+
+# I should convert tkrprices_df into a CSV string:
+csv0_s = tkrprices_df.to_csv(index=False)
+csv_s  = "'"+csv0_s+"'"
+
+sql_s = "insert into tkrprices(csv)values("+csv_s+")"
+conn.execute(sql_s)
+
+sql_s = "select csv from tkrprices limit 1"
+result = conn.execute(sql_s)
+print(result)
+
+# <sqlalchemy.engine.result.ResultProxy object at 0x7fcbccdade80>
+
+for row in result:
+    print(row['csv'])
