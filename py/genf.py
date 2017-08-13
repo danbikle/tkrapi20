48b4d79d38015161226afa7bd84f05a45d2b24e1
Dan Bikle <danbikle@outlook.com>
2017-08-12 17:37:50 -0700

    more

--- py/genf.py
+++ py/genf.py
@@ -8,11 +8,20 @@ Demo:
 """
 
 import pdb
-import pandas as pd
-from sqlalchemy import create_engine
+import pandas     as pd
+import sqlalchemy as sql
 
-# I should connect to the DB
+# I should connect to the DB:
 db_s = 'postgres://tkrapi:tkrapi@127.0.0.1/tkrapi'
-conn = create_engine(db_s).connect()
+conn = sql.create_engine(db_s).connect()
 
+# I should loop through the tkrprices table:
+sql_s   = "select tkr,csv from tkrprices WHERE tkr like 'AA%' order by tkr"
+sql_sql = sql.text(sql_s)
+result  = conn.execute(sql_sql)
+if not result.rowcount:
+  sys.exit(1)
+for row in result:
+    print(row.tkr)
+    
 'bye'
