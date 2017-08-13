93e600c9f216c434957b46b925e9ebc9257cb3ca
Dan Bikle <danbikle@outlook.com>
2017-08-13 13:07:03 -0700

    more

--- py/genf.py
+++ py/genf.py
@@ -13,9 +13,17 @@ import numpy      as np
 import pandas     as pd
 import sqlalchemy as sql
 
+print('Busy generating features...')
+
 # I should connect to the DB:
 db_s = 'postgres://tkrapi:tkrapi@127.0.0.1/tkrapi'
 conn = sql.create_engine(db_s).connect()
+
+sql_s = "drop table if exists features"
+conn.execute(sql_s)
+
+sql_s = "create table features(tkr varchar, csv text)"
+conn.execute(sql_s)
 
 # I should loop through the tkrprices table:
 sql_s   = "select tkr,csv from tkrprices WHERE tkr like 'AA%' order by tkr"
@@ -25,10 +33,6 @@ if not result.rowcount:
   sys.exit(1)
 for row in result:
     print(row.tkr)
-    pdb.set_trace()
-    'bye'
-def nada():
-
     # I should convert each row into a DataFrame so I can generate features:
     feat_df = pd.read_csv(io.StringIO(row.csv),names=('cdate','cp'))
     # But first, I should get the dependent variable:
@@ -41,5 +45,16 @@ def nada():
     # debug
     feat_df.to_csv('/tmp/tmp.csv',index=False, float_format="%.3f")
     # debug
+    # I should convert to String to move it towards the db:
+    csv0_s = feat_df.to_csv(index=False,header=True,float_format='%.3f')
+    csv_s  = "'"+csv0_s+"'"
+    tkr_s  = "'"+row.tkr+"'"
+    sql_s  = "insert into features(tkr,csv)values("+tkr_s+","+csv_s+")"
+    conn.execute(sql_s)
+# I should check:
+# ../bin/psql.bash
+# select       tkr  from features;
+# select count(tkr) from features;
+# select csv from features where tkr='FB';
     
 'bye'
