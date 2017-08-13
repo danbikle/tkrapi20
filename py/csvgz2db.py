cf5de6715cafeb9b27c049adbe2c91452c54da66
Dan Bikle <danbikle@outlook.com>
2017-08-12 17:26:17 -0700

    more

--- py/csvgz2db.py
+++ py/csvgz2db.py
@@ -11,7 +11,6 @@ import glob
 import pandas as pd
 from sqlalchemy import create_engine
 
-
 # I should connect to the DB
 db_s = 'postgres://tkrapi:tkrapi@127.0.0.1/tkrapi'
 conn = create_engine(db_s).connect()
--- py/genf.py
+++ py/genf.py
@@ -7,4 +7,12 @@ Demo:
 ~/anaconda3/bin/python genf.py
 """
 
+import pdb
+import pandas as pd
+from sqlalchemy import create_engine
+
+# I should connect to the DB
+db_s = 'postgres://tkrapi:tkrapi@127.0.0.1/tkrapi'
+conn = create_engine(db_s).connect()
+
 'bye'
