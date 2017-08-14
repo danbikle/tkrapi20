04fc373f875aa26e42174eeada4c03001c2d87d9
Dan Bikle <danbikle@outlook.com>
2017-08-14 15:06:01 -0700

    more

---
+++ py/unsplit_prices.py
@@ -0,0 +1,17 @@
+"""
+unsplit_prices.py
+
+This script should create a column named uscp which I see as unsplit_prices.
+
+Given a splitdate, this script should find all prices at and after the splitdate.
+Then uscp should be cp * split_ratio.
+
+Demo:
+python unsplit_prices.py
+"""
+
+import pdb
+import pandas as pd
+from   fractions import Fraction
+
+'bye'
