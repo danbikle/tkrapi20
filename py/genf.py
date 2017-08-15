e3099ef7d7d7261b5d9f0f716b0de1905819f798
Dan Bikle <danbikle@outlook.com>
2017-08-14 21:02:22 -0700

    more

--- py/genf.py
+++ py/genf.py
@@ -4,10 +4,12 @@ genf.py
 This script should generate features from dates and prices.
 
 Demo:
-~/anaconda3/bin/python genf.py
+. env.bash
+$PYTHON genf.py
 """
 
 import io
+import os
 import pdb
 import numpy      as np
 import pandas     as pd
@@ -16,7 +18,7 @@ import sqlalchemy as sql
 print('Busy generating features...')
 
 # I should connect to the DB:
-db_s = 'postgres://tkrapi:tkrapi@127.0.0.1/tkrapi'
+db_s = os.environ['PGURL']
 conn = sql.create_engine(db_s).connect()
 
 sql_s = "drop table if exists features"
@@ -26,35 +28,35 @@ sql_s = "create table features(tkr varchar, csv text)"
 conn.execute(sql_s)
 
 # I should loop through the tkrprices table:
-sql_s   = "select tkr,csvh from tkrprices WHERE tkr like 'AA%' order by tkr"
+sql_s   = "select tkr,csvh from tkrprices order by tkr"
 sql_sql = sql.text(sql_s)
 result  = conn.execute(sql_sql)
 if not result.rowcount:
   sys.exit(1)
 for row in result:
-    print(row.tkr)
-    # I should convert each row into a DataFrame so I can generate features:
-    feat_df = pd.read_csv(io.StringIO(row.csvh),names=('cdate','cp'))
-    # But first, I should get the dependent variable:
-    feat_df['pct_lead'] = 100.0*((feat_df.cp.shift(-1) - feat_df.cp) / feat_df.cp).fillna(0)
-    # Now, I should get features:
-    feat_df['pct_lag1'] = 100.0*((feat_df.cp - feat_df.cp.shift(1))/feat_df.cp.shift(1)).fillna(0)
-    feat_df['pct_lag2'] = 100.0*((feat_df.cp - feat_df.cp.shift(2))/feat_df.cp.shift(2)).fillna(0)
-    feat_df['pct_lag4'] = 100.0*((feat_df.cp - feat_df.cp.shift(4))/feat_df.cp.shift(4)).fillna(0)
-    feat_df['pct_lag8'] = 100.0*((feat_df.cp - feat_df.cp.shift(8))/feat_df.cp.shift(8)).fillna(0)
-    # debug
-    feat_df.to_csv('/tmp/tmp.csv',index=False, float_format="%.3f")
-    # debug
-    # I should convert to String to move it towards the db:
-    csv0_s = feat_df.to_csv(index=False,header=True,float_format='%.3f')
-    csv_s  = "'"+csv0_s+"'"
-    tkr_s  = "'"+row.tkr+"'"
-    sql_s  = "insert into features(tkr,csv)values("+tkr_s+","+csv_s+")"
-    conn.execute(sql_s)
+  print(row.tkr)
+  # I should convert each row into a DataFrame so I can generate features:
+  feat_df = pd.read_csv(io.StringIO(row.csvh),names=('cdate','cp'))
+  # But first, I should calculate the dependent variable:
+  feat_df['pct_lead'] = 100.0*((feat_df.cp.shift(-1) - feat_df.cp) / feat_df.cp).fillna(0)
+  # Now, I should get features:
+  feat_df['pct_lag1'] = 100.0*((feat_df.cp - feat_df.cp.shift(1))/feat_df.cp.shift(1)).fillna(0)
+  feat_df['pct_lag2'] = 100.0*((feat_df.cp - feat_df.cp.shift(2))/feat_df.cp.shift(2)).fillna(0)
+  feat_df['pct_lag4'] = 100.0*((feat_df.cp - feat_df.cp.shift(4))/feat_df.cp.shift(4)).fillna(0)
+  feat_df['pct_lag8'] = 100.0*((feat_df.cp - feat_df.cp.shift(8))/feat_df.cp.shift(8)).fillna(0)
+  # debug
+  feat_df.to_csv('/tmp/tmp.csv',index=False, float_format="%.3f")
+  # debug
+  # I should convert to String to move it towards the db:
+  csv0_s = feat_df.to_csv(index=False,header=True,float_format='%.3f')
+  csv_s  = "'"+csv0_s+"'"
+  tkr_s  = "'"+row.tkr+"'"
+  sql_s  = "insert into features(tkr,csv)values("+tkr_s+","+csv_s+")"
+  conn.execute(sql_s)
 # I should check:
 # ../bin/psql.bash
 # select       tkr  from features;
 # select count(tkr) from features;
-# select csv from features where tkr='FB';
+# select tkr,csv from features limit 1;
 
 'bye'
