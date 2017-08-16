04a67295337ce69f3cf95878fb7f8ce992763ffc
Dan Bikle <danbikle@outlook.com>
2017-08-16 00:44:34 -0700

    more

--- flaskr.py
+++ flaskr.py
@@ -122,28 +122,9 @@ def learn_predict_sklinear(tkr='ABC',yrs=20,mnth='2016-11', features='pct_lag1,s
   linr_model = skl.LinearRegression()
   pdb.set_trace()
   xtrain_a, ytrain_a, xtest_a, out_df = get_train_test(tkr,yrs,mnth,features)
-  # I should get features for this tkr from db:
-  feat_df = getfeat(tkr)
-  # I should get the test data from feat_df:
-  test_bool_sr = (feat_df.cdate.str[:7] == mnth)
-  test_df      =  feat_df.loc[test_bool_sr] # should be about 21 rows
-  # I should get the training data from feat_df:
-  max_train_loc_i = -1 + test_df.index[0]
-  min_train_loc_i = max_train_loc_i - yrs * 252
-  if (min_train_loc_i < 10):
-    min_train_loc_i = 10
-  train_df = feat_df.loc[min_train_loc_i:max_train_loc_i]
-
-  features_l = features.split(',')
-  xtrain_df  = train_df[features_l]
-  xtrain_a   = np.array(xtrain_df)
-  ytrain_a   = np.array(train_df)[:,2 ]
-
-  xtest_df = test_df[features_l]
-  xtest_a  = np.array(xtest_df)
-  out_df   = test_df.copy()[['cdate','cp','pct_lead']]
-  
+  # I should fit a model to xtrain_a, ytrain_a
   linr_model.fit(xtrain_a,ytrain_a)
+  # I should predict xtest_a then update out_df
   out_df['prediction']    = np.round(linr_model.predict(xtest_a),3).tolist()
   out_df['effectiveness'] = np.sign(out_df.pct_lead*out_df.prediction)*np.abs(out_df.pct_lead)
   out_df['accuracy']      = (1+np.sign(out_df.effectiveness))/2
