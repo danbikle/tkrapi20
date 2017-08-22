3f0f8eb981fb5a874d764c434c1f35e0d5b25933
Dan Bikle <danbikle@outlook.com>
2017-08-21 17:56:29 -0700

    more

--- dev.py
+++ dev.py
@@ -8,19 +8,86 @@ Demo:
 $PYTHON dev.py
 """
 
+
 import io
 import pdb
 import os
 import flask
+import datetime      as dt
 import flask_restful as fr
-import sqlalchemy as sql
-import pandas as pd
+import numpy         as np
+import pandas        as pd
+import sqlalchemy    as sql
+import keras
+# modules in the py folder:
 
-import flaskr
 
 import pgdb
 import kerastkr
 import sktkr
+
+# https://keras.io/models/model/#methods
+batch_size_i = 256 # Doc: Number of samples per gradient update.
+epochs_i     = 1 # Doc: Number of epochs to train the model.
+
+# I should create a simple model.
+
+def learn_predict_keraslinear(tkr='ABC',yrs=20,mnth='2016-11', features='pct_lag1,slope4,moy'):
+  """This function should use keras to learn, predict."""
+  # I should get train, test data.
+  # Also get copy of test data in a DataFrame for later reporting:
+  xtrain_a, ytrain_a, xtest_a, out_df = pgdb.get_train_test(tkr,yrs,mnth,features)
+  if ((xtrain_a.size == 0) or (ytrain_a.size == 0) or (xtest_a.size == 0)):
+    return out_df # probably empty too.
+  # Start using Keras here.
+  kmodel     = keras.models.Sequential()
+  # I should fit a Keras model to xtrain_a, ytrain_a
+  features_l = features.split(',')
+  features_i = len(features_l)
+  kmodel.add(keras.layers.core.Dense(features_i, input_shape=(features_i,)))
+  # https://keras.io/activations/
+  kmodel.add(keras.layers.core.Activation('linear'))
+  # I should have 1 linear-output:
+  kmodel.add(keras.layers.core.Dense(1)) 
+  kmodel.add(keras.layers.core.Activation('linear'))
+  kmodel.compile(loss='mean_squared_error', optimizer='adam')
+  kmodel.fit(xtrain_a,ytrain_a, batch_size=batch_size_i, epochs=epochs_i)
+  pdb.set_trace()
+  # I should save the model:
+  # https://keras.io/getting-started/faq/#how-can-i-save-a-keras-model
+  import tempfile
+  with tempfile.NamedTemporaryFile() as fp:
+    pdb.set_trace()
+    kmodel.save(fp.name)
+    fp.seek(0)
+    somebytes = fp.read()
+    print(len(somebytes))
+
+    # Use the codecs module to encode
+    import codecs
+    base64_data = codecs.encode(somebytes, 'base64')
+    print(base64_data[:99])
+
+  # I should predict xtest_a then update out_df
+  predictions_a           = np.round(kmodel.predict(xtest_a),3)
+  # Done with Keras, I should pass along the predictions.
+  predictions_l           = [p_f[0] for p_f in predictions_a] # I want a list
+  out_df['prediction']    = predictions_l
+  out_df['effectiveness'] = np.sign(out_df.pct_lead*out_df.prediction)*np.abs(out_df.pct_lead)
+  out_df['accuracy']      = (1+np.sign(out_df.effectiveness))/2
+  algo                    = 'keraslinear'
+  pgdb.predictions2db(tkr,yrs,mnth,features,algo,out_df)
+  # I should return a DataFrame useful for reporting on the predictions.
+  return out_df
+
+pdb.set_trace()
+out_df = learn_predict_keraslinear()
+
+# I should save it to file(s).
+
+# I should use it to predict.
+
+stophere
 
 valid_s = set([1,2,3,4,5])
 
