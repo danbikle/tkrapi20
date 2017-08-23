46b0afc51e5b6e64238aaa69eefff641ddd1d16b
Dan Bikle <danbikle@outlook.com>
2017-08-22 18:38:44 -0700

    more

--- py/demokeras.py
+++ py/demokeras.py
@@ -11,9 +11,10 @@ import keras
 import numpy as np
 import pdb
 
-x_l = [1.1, 2.2]
-x_a = np.array((x_l,x_l,x_l,x_l,x_l,x_l))
-
+x0_a = np.array([1.1, 2.2])
+x_a  = np.array((0+x0_a, 1+x0_a, 2+x0_a, 3+x0_a, 4+x0_a))
+y_a  = np.array((1.3,2.1,3.4,4.0,5.2)).reshape((-1,1))
 print(x_a)
+print(y_a)
 
 'bye'
