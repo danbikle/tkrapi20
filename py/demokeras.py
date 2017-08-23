bfdeb5e53feeb242f9ed6f796b0ca876679ff0e5
Dan Bikle <danbikle@outlook.com>
2017-08-22 18:29:25 -0700

    more

---
+++ py/demokeras.py
@@ -0,0 +1,19 @@
+"""
+demokeras.py
+
+This script should demo keras.
+
+Demo:
+~/anaconda3/bin/python demokeras.py
+"""
+
+import keras
+import numpy as np
+import pdb
+
+x_l = [1.1, 2.2]
+x_a = np.array((x_l,x_l,x_l,x_l,x_l,x_l))
+
+print(x_a)
+
+'bye'
